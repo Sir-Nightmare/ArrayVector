b9af4a9149c94d3b6c90bbd903aab1c9821ab868
Vanya <vanya@gmail.com>
2016-07-18 21:27:47 +0300

    Initial commit

---
+++ src/ru/ncedu/java/tasks/ArrayVectorImpl.java
@@ -0,0 +1,93 @@
+package ru.ncedu.java.tasks;
+
+/**
+ * Created by Sir Nightmare on 7/16/2016.
+ */
+public class ArrayVectorImpl implements ArrayVector
+{
+
+    /*
+    Your class must have a public no-argument constructor that means either the default constructor (the absence of any constructors in your code) or the presence of the following code:
+   public ArrayVectorImpl(){ ... }
+
+Your class must not have compilation errors (write your code in IDE).
+
+Instantiation with the no-argument constructor must not fail (it's recommended to test it in main() method).
+     */
+    double[] arr;
+    int numOfCoordinates=0;
+
+    public ArrayVectorImpl() {
+        this(0,0);
+    }
+    public ArrayVectorImpl(double... elements) {
+        arr=elements;
+        numOfCoordinates=arr.length;
+    }
+
+    @Override
+    public void set(double... elements) {
+
+    }
+
+    @Override
+    public double[] get() {
+        return new double[0];
+    }
+
+    @Override
+    public ArrayVector clone() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void set(int index, double value) {
+
+    }
+
+    @Override
+    public double get(int index) throws ArrayIndexOutOfBoundsException {
+        return 0;
+    }
+
+    @Override
+    public double getMax() {
+        return 0;
+    }
+
+    @Override
+    public double getMin() {
+        return 0;
+    }
+
+    @Override
+    public void sortAscending() {
+
+    }
+
+    @Override
+    public void mult(double factor) {
+
+    }
+
+    @Override
+    public ArrayVector sum(ArrayVector anotherVector) {
+        return null;
+    }
+
+    @Override
+    public double scalarMult(ArrayVector anotherVector) {
+        return 0;
+    }
+
+    @Override
+    public double getNorm() {
+        return 0;
+    }
+
+}
---
+++ src/ru/ncedu/java/tasks/Main.java
@@ -0,0 +1,10 @@
+package ru.ncedu.java.tasks;
+
+/**
+ * Created by Sir Nightmare on 7/17/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
