e380bf62557aeada76dfcac286750384985707f6
Sir Nightmare <ivantochilkin@gmail.com>
2016-07-25 18:15:55 +0300

    class was made

--- src/ru/ncedu/java/tasks/ArrayVectorImpl.java
+++ src/ru/ncedu/java/tasks/ArrayVectorImpl.java
@@ -1,5 +1,9 @@
 package ru.ncedu.java.tasks;
 
+import java.util.Arrays;
+
+import static java.lang.Math.sqrt;
+
 /**
  * Class for vector
  */
@@ -33,6 +37,7 @@ Instantiation with the no-argument constructor must not fail (it's recommended t
 
     @Override
     public double[] get() {
+
         return arr;
     }
 
@@ -46,6 +51,7 @@ Instantiation with the no-argument constructor must not fail (it's recommended t
 
     @Override
     public int getSize() {
+
         return numOfCoordinates;
     }
 
@@ -54,27 +60,27 @@ Instantiation with the no-argument constructor must not fail (it's recommended t
         if (index > 0) {
             if (index < numOfCoordinates) {
                 arr[index] = value;
-            } else{
-                double[] newArray = new double[index+1];
+            } else {
+                double[] newArray = new double[index + 1];
                 System.arraycopy(newArray, 0, arr, 0, numOfCoordinates);
-                newArray[index]=value;
-                arr=newArray;
-                newArray=null;
+                newArray[index] = value;
+                arr = newArray;
+                newArray = null;
             }
         }
     }
 
     @Override
     public double get(int index) throws ArrayIndexOutOfBoundsException {
-        return 0;
+        return arr[index];
     }
 
     @Override
     public double getMax() {
-        double tempMax=arr[0];
-        for(int i=1;i<numOfCoordinates;i++) {
-            if(arr[i]-tempMax>1e-10){
-                tempMax=arr[i];
+        double tempMax = arr[0];
+        for (int i = 1; i < numOfCoordinates; i++) {
+            if (arr[i] - tempMax > 1e-10) {
+                tempMax = arr[i];
             }
         }
         return tempMax;
@@ -82,10 +88,10 @@ Instantiation with the no-argument constructor must not fail (it's recommended t
 
     @Override
     public double getMin() {
-        double tempMin=arr[0];
-        for(int i=1;i<numOfCoordinates;i++) {
-            if(tempMin-arr[i]>1e-10){
-                tempMin=arr[i];
+        double tempMin = arr[0];
+        for (int i = 1; i < numOfCoordinates; i++) {
+            if (tempMin - arr[i] > 1e-10) {
+                tempMin = arr[i];
             }
         }
         return tempMin;
@@ -93,27 +99,39 @@ Instantiation with the no-argument constructor must not fail (it's recommended t
 
     @Override
     public void sortAscending() {
-
+        Arrays.sort(arr);
     }
 
     @Override
     public void mult(double factor) {
-
+        for (int i = 1; i < numOfCoordinates; i++) {
+            arr[i] *= factor;
+        }
     }
 
     @Override
     public ArrayVector sum(ArrayVector anotherVector) {
+        for (int i = 1; i < numOfCoordinates && i < anotherVector.getSize(); i++) {
+            arr[i] += anotherVector.get(i);
+        }
+
         return this;
     }
 
     @Override
     public double scalarMult(ArrayVector anotherVector) {
-        return 0;
+        double scalarMultResult = 0;
+        for (int i = 1; i < numOfCoordinates && i < anotherVector.getSize(); i++) {
+            scalarMultResult += arr[i] * anotherVector.get(i);
+        }
+
+        return scalarMultResult;
     }
 
     @Override
     public double getNorm() {
-        return 0;
+        double normResult = sqrt(scalarMult(this));
+        return normResult;
     }
 
 }
--- src/ru/ncedu/java/tasks/Main.java
+++ src/ru/ncedu/java/tasks/Main.java
@@ -5,6 +5,20 @@ package ru.ncedu.java.tasks;
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println ("Hello World!");
+        ArrayVector two = new ArrayVectorImpl(3,2,1,0);
+        double[] array1 = two.get();
+        ArrayVector one = two.clone();
+        for (int i = 0; i < two.getSize(); i++) {
+            System.out.print(two.get(i));
+            System.out.print(' ');
+            }
+
+        System.out.println(' ');
+        for (int i = 0; i < one.getSize(); i++) {
+            System.out.print(one.get(i));
+            System.out.print(' ');
+        }
 
     }
 }
